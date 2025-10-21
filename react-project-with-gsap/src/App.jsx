65f5506cb44a8e3cb68df3c3b1725627d47420a4
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-10-21 11:26:59 +0600

    react project with gsap

--- react-project-with-gsap/src/App.jsx
+++ react-project-with-gsap/src/App.jsx
@@ -2,6 +2,10 @@
 // import gsap from "gsap";
 // import { useRef } from "react";
 
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const App = () => {
   // useGSAP(() => {
   //   gsap.to(".box", {
@@ -49,6 +53,51 @@ const App = () => {
   //   { scope: container }
   // );
 
+  // const [circle, setCircle] = useState(0);
+  // const random = gsap.utils.random(-500, 500, 100);
+
+  // useGSAP(() => {
+  //   gsap.to(".circle", {
+  //     x: circle,
+  //     duration: 0.5,
+  //   });
+  // }, [circle]);
+
+  // const randomX = gsap.utils.random(-500, 500, 100);
+  // const randomY = gsap.utils.random(-200, 200, 30);
+  // const rotateX = gsap.utils.random(-360, 360, 30);
+
+  // const [xValue, setXValue] = useState(0);
+  // const [yValue, setYValue] = useState(0);
+  // const [rotateValue, setRotateValue] = useState(0);
+
+  // const imageRef = useRef();
+
+  // useGSAP(
+  //   () => {
+  //     gsap.to(imageRef.current, {
+  //       x: xValue,
+  //       y: yValue,
+  //       duration: 1,
+  //       rotate: rotateValue,
+  //     });
+  //   },
+  //   {
+  //     scope: "main",
+  //     dependencies: [xValue, rotateX, yValue],
+  //   }
+  // );
+
+  const boxRef = useRef();
+  const { contextSafe } = useGSAP();
+
+  const rotateBox = contextSafe(() => {
+    gsap.to(boxRef.current, {
+      rotate: 360,
+      duration: 1,
+    });
+  });
+
   return (
     <main>
       {/* <div ref={gsapRef} className="box"></div> */}
@@ -62,7 +111,33 @@ const App = () => {
         <div className="circle"></div>
       </div> */}
 
-      <button>Move</button>
+      {/* <button
+        onClick={() => {
+          // setCircle(random);
+          setXValue(randomX);
+          setRotateValue(rotateX);
+          setYValue(randomY);
+        }}
+      >
+        Animate
+      </button> */}
+      {/* <div className="circle"></div> */}
+      {/* <div ref={imageRef} className="squareBox"></div> */}
+      {/* <img
+        onClick={() => {
+          // setCircle(random);
+          setXValue(randomX);
+          setRotateValue(rotateX);
+          setYValue(randomY);
+        }}
+        ref={imageRef}
+        src="https://img.favpng.com/24/16/5/bird-flying-colorful-bird-in-flight-00fJPR1w_t.jpg"
+        alt=""
+      /> */}
+      <button onClick={rotateBox}>Animate</button>
+      <div ref={boxRef} className="squareBox">
+        Box
+      </div>
     </main>
   );
 };
