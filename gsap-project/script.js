a762b9b2f5d37c6ac747e28a7de3b84d2a36a8e8
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-10-22 22:33:34 +0600

    scroll trigger

--- gsap-project/script.js
+++ gsap-project/script.js
@@ -42,3 +42,82 @@
 //   backgroundColor: "blue",
 //   //   borderRadius: "50%",
 // });
+
+// gsap.from("h3, h1, h4", {
+//   opacity: 0,
+//   y: 30,
+//   duration: 2,
+//   delay: 1,
+//   stagger: 1,
+// });
+
+// gsap.from(".box", {
+//   x: 1200,
+//   duration: 1,
+//   delay: 1,
+//   rotate: 720,
+//   //   repeat: 3,
+//   repeat: -1,
+//   yoyo: true,
+// });
+
+// gsap.to(".box1", {
+//   x: 1500,
+//   duration: 1.5,
+//   delay: 1,
+// });
+// gsap.to(".box2", {
+//   x: 1500,
+//   duration: 1.5,
+//   rotate: 720,
+//   delay: 2.5,
+// });
+// gsap.to(".box3", {
+//   x: 1500,
+//   duration: 1.5,
+//   delay: 4,
+//   borderRadius: "50%",
+// });
+
+// ** Timeline
+// const tl = gsap.timeline();
+
+// tl.to(".box1", {
+//   x: 1500,
+//   duration: 1.5,
+// });
+// tl.to(".box2", {
+//   x: 1500,
+//   duration: 1.5,
+//   rotate: 720,
+// });
+// tl.to(".box3", {
+//   x: 1500,
+//   duration: 1.5,
+//   borderRadius: "50%",
+// });
+
+// const tl = gsap.timeline();
+
+// tl.from("h2", {
+//   y: -30,
+//   opacity: 0,
+//   duration: 0.5,
+//   delay: 0.3,
+// });
+// tl.from("li", {
+//   y: -30,
+//   opacity: 0,
+//   duration: 0.5,
+//   delay: 0.3,
+//   stagger: 0.3,
+// });
+
+// tl.from("h1", {
+//   y: 20,
+//   opacity: 0,
+//   duration: 0.5,
+//   scale: 0.2,
+// });
+
+//** Scroll Trigger
