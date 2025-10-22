da49b937ff95eebf999d7d419d93e0bbf2faddbc
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-10-22 11:17:53 +0600

    gsap complete animated course

---
+++ gsap-project/script.js
@@ -0,0 +1,44 @@
+// gsap.to("#box", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 0.5,
+//   //   opacity: 0,
+// });
+
+// gsap.to("#box1", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+// });
+
+// gsap.to("#box2", {
+//   x: 500,
+//   y: 500,
+//   duration: 2,
+//   delay: 1,
+// });
+
+// gsap.to("#box1", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+// });
+// gsap.from("#box1", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+// });
+// gsap.from("#box2", {
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+// });
+
+// gsap.to("#box1", {
+//   x: 1200,
+//   duration: 1.5,
+//   delay: 0.5,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   //   borderRadius: "50%",
+// });
